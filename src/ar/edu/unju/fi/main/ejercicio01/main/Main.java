b3487c5a4880c11a96c067bb34fdeb3d5dd47458
ezequ <ezequ@DESKTOP-3S964OD>
2024-04-25 15:24:07 -0300

    1era Parte del Ejercicio01 en clase

--- src/ar/edu/unju/fi/main/ejercicio01/main/Main.java
+++ src/ar/edu/unju/fi/main/ejercicio01/main/Main.java
@@ -1,9 +1,17 @@
 package ar.edu.unju.fi.main.ejercicio01.main;
+import ar.edu.unju.fi.main.ejercicio01.model.*;
+import ar.edu.unju.fi.main.ejercicio01.model.Producto.Categoria;
+import ar.edu.unju.fi.main.ejercicio01.model.Producto.OrigenFabricacion;
 
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<Producto> listado = new ArrayList<Producto>();
+		Producto prod01=new Producto("p01","caracteristica p01",23,OrigenFabricacion.BRASIL,Categoria.INFORMATICA);
+		System.out.println(prod01.toString());
+		
 
 	}
 
--- src/ar/edu/unju/fi/main/ejercicio01/model/Producto.java
+++ src/ar/edu/unju/fi/main/ejercicio01/model/Producto.java
@@ -1,9 +1,32 @@
 package ar.edu.unju.fi.main.ejercicio01.model;
 
 public class Producto {
+	
+	public enum OrigenFabricacion{
+		ARGENTINA,CHINA,BRASIL, URUGUAY,}
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS,
+	}
 	private String codigo;
 	private String descripcion;
 	private double precioU;
+	private OrigenFabricacion origenFabricacion  ;
+	private Categoria  categoria;
+	
+	
+	public Producto(String codigo, String descripcion, double precioU,OrigenFabricacion origenFabricacion , Categoria categoria) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioU = precioU;
+		this.origenFabricacion = origenFabricacion;
+		this.categoria= categoria;
+	}
+	
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descripcion=" + descripcion + ", precioU=" + precioU
+				+ ", origenFabricacion=" + origenFabricacion + "]";
+	}
 	public String getCodigo() {
 		return codigo;
 	}
